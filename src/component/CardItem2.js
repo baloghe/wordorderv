ab41214f0ff91a77022f4cfbf9603a0fa71a4662
Endre BALOGH <balogh_endre@gmail.hu>
2023-06-02 12:11:51 +0200

    CardItem2 (clickable version) added

---
+++ src/component/CardItem2.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./CardItem.css";
+
+/**
+  Clickable version (fits better handheld devices)
+*/
+export default class CardItem2 extends React.Component {
+	constructor(props){
+  	super(props);
+  }
+  
+  cardClicked = e => {
+  	if(!this.props.clickEnabled)
+    	return;
+  	let clk = this.props.listitemkey;
+    this.props.cardClicked(clk);
+  }
+  
+	render() {
+  	return (
+      <div className={this.props.wordClasses}
+          key={this.props.listitemkey}
+          data-itemkey={this.props.listitemkey}
+          onClick={this.cardClicked}
+        >
+        {this.props.word}
+      </div>
+      );
+  };
+  
+}
